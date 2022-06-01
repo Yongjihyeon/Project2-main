84aa4634ca5fcc02fa778648645be97bd592df60
jihyeon Yong <doyle33323@gmail.com>
2022-06-01 18:26:21 +0900

    Update MainActivity.java

--- app/src/main/java/com/android/project2/MainActivity.java
+++ app/src/main/java/com/android/project2/MainActivity.java
@@ -29,11 +29,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // calendar.db 생성 및 테이블 생성
-        /*DBHelper helper = new DBHelper(MainActivity.this, "calendar.db", null, 1);
-        SQLiteDatabase db = helper.getWritableDatabase();
-        helper.onCreate(db);*/
-
         getSupportFragmentManager().beginTransaction().add(R.id.frameLayout, new MonthFragment()).commit();
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -56,49 +51,13 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("date", date);
                 intent.putExtra("hour", hour);
                 startActivityForResult(intent, 0);
-/*
-                // DB로부터 그 연,월,일에 해당하는 일정들 가져옴
-                // 만약 시간 정보가 존재한다면 (주간 달력일 경우) DB에서 데이터를 가져올 때 시간 정보도 추가
-                ArrayList<Schedule> scheduleList = hour == -1
-                        ? helper.getSchedules("sch_year", year, "sch_month", month, "sch_date", date)                       // 월간 달력일 경우
-                        : helper.getSchedules("sch_year", year, "sch_month", month, "sch_date", date, "start_hour", hour);  // 주간 달력일 경우
 
-                if ( scheduleList.size() > 1 ) {    // 가져온 데이터의 길이가 1 초과일 경우
-
-                    // 다이얼로그에 채우기 위한 schedules 배열 생성 및 초기화
-                    String[] schedules = new String[scheduleList.size()];
-                    for (int i=0; i<scheduleList.size(); i++) schedules[i] = scheduleList.get(i).title;
-
-                    // 일정들의 제목이 적힌 다이얼로그 띄움
-                    AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-                    dialog.setTitle(year + "." + month + "." + date + "일");
-                    dialog.setItems(schedules, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            // 다이얼로그의 항목을 누를 경우 일정의 id값을 intent에 추가
-                            intent.putExtra("_id", scheduleList.get(i)._id);
-                            startActivityForResult(intent, 0);
-                        }
-                    });
-                    dialog.show();
-                } else if ( scheduleList.size() == 1 ) {    // 가져온 데이터가 1개일 경우 리스트의 0번째 항목의 id값을 intent에 추가
-                    intent.putExtra("_id", scheduleList.get(0)._id);
-                    startActivityForResult(intent, 0);
-                } else {
-                    //가져온 데이터가 없으면 intent에 id값 추가하지 않음
-                    startActivityForResult(intent, 0);
-                }*/
             }
         });
     }
 
     @Override
-    /*protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        FragmentTransaction ft = fragment.getFragmentManager().beginTransaction();
-        ft.detach(fragment).attach(fragment).commit();
 
-    }*/
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // 앱바의 옵션 버튼을 누를 경우 mainmenu.xml의 레이아웃을 보여줌
