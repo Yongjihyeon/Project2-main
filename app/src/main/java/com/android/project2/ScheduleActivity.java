19d31c560cc351703024adbd9a71a6b0cb3e77f8
jihyeon Yong <doyle33323@gmail.com>
2022-06-03 03:20:42 +0900

    수정

--- app/src/main/java/com/android/project2/ScheduleActivity.java
+++ app/src/main/java/com/android/project2/ScheduleActivity.java
@@ -41,11 +41,10 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
     // 수정할 일정 객체
     private Schedule schedule;
 
-    // 수정하는 연,월,일
+    // 수정하는 연도 ,월,일
     private int year;
     private int month;
     private int date;
-
     // 제목
     private EditText titleEditText;
     // 시작시간
@@ -92,7 +91,7 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
 
         // startHour 변수를 입력받은 시간이 있으면 (=주간 달력에서 넘어온 경우) 해당 시간으로, 없으면 현재 시간으로 설정
         int startHour = intent.getIntExtra("hour", -1);
-        if ( startHour == -1 ) startHour = calendar.get(Calendar.HOUR_OF_DAY);
+        if (startHour == -1) startHour = calendar.get(Calendar.HOUR_OF_DAY);
         // endHour는 startHour의 1시간 뒤
         int endHour = (startHour + 1) % 24;
 
@@ -110,7 +109,7 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
         Button cancelButton = findViewById(R.id.schedule_cancel_btn);
         Button delButton = findViewById(R.id.schedule_del_btn);
 
-        // 저장 버튼 누르면
+        // 저장 버튼
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -118,9 +117,9 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
             }
         });
 
-        //취소 버튼 누르면 액티비티 종료
+        //취소 버튼
         cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
+
             public void onClick(View view) {
                 ScheduleActivity.this.finish();
             }
@@ -137,46 +136,80 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
         placeFindButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mGoogleMap != null) {
-                    LatLng location = new LatLng(37.5882827, 127.006390);
-                    mGoogleMap.addMarker(
-                            new MarkerOptions().
-                                    position(location).
-                                    title("한성대입구역").
-                                    alpha(0.8f).
-                                    snippet("4호선")
-                    );
-                    mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,15));
-                }
+
             }
         });
 
     }
 
-    @Override
-  public void onMapReady(GoogleMap googleMap) {
-      mGoogleMap = googleMap;
 
-      LatLng hansung = new LatLng(37.5817891, 127.009854);
-      googleMap.addMarker(
-              new MarkerOptions().
-                      position(hansung).
-                      title("한성대학교"));
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        geocoder = new Geocoder(this);
 
-      // move the camera
-      googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hansung,15));
+        LatLng hansung = new LatLng(37.5817891, 127.009854);
+        googleMap.addMarker(
+                new MarkerOptions().
+                        position(hansung).
+                        title("한성대학교"));
 
-      mGoogleMap.setOnMarkerClickListener(new MyMarkerClickListener());
-  }
+        // move the camera
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hansung, 15));
 
-            class MyMarkerClickListener implements GoogleMap.OnMarkerClickListener {
-
-                @Override
-                public boolean onMarkerClick(Marker marker) {
-                    if (marker.getTitle().equals("한성대입구역")) {
-                        Toast.makeText(getApplicationContext(),"한성대입구역을 선택하셨습니다", Toast.LENGTH_SHORT).show();
-                    }
-                    return false;
+        mGoogleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                if (marker.getTitle().equals("한성대입구역")) {
+                    Toast.makeText(getApplicationContext(), "한성대입구역을 선택하셨습니다", Toast.LENGTH_SHORT).show();
                 }
+                return false;
             }
-        }
+        });
+
+        // 장소 찾기 버튼 이벤트 설정
+        placeFindButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String place = placeEditText.getText().toString();
+                List<Address> addressList = null;
+                try {
+                    // editText에 입력한 텍스트(주소, 지역, 장소 등)을 지오 코딩을 이용해 변환
+                    addressList = geocoder.getFromLocationName(
+                            place, // 주소
+                            1 // 최대 검색 결과 개수
+                    );
+                } catch (IOException e) {
+                    Log.e("[[ScheduleActivity]]", e.getMessage());
+                }
+
+                if (addressList.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "검색 결과가 없습니다.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // 콤마를 기준으로 split
+                String[] splitStr = addressList.get(0).toString().split(",");
+                String address = splitStr[0].substring(splitStr[0].indexOf("\"") + 1, splitStr[0].length() - 2); // 주소
+
+                // 검색결과 문자열 예시:
+                // Address[addressLines=[0:"Seoul, South Korea"],feature=Seoul,admin=Seoul,sub-admin=null,locality=null,thoroughfare=null,postalCode=null,countryCode=KR,countryName=South Korea,hasLatitude=true,latitude=37.566535,hasLongitude=true,longitude=126.97796919999999,phone=null,url=null,extras=null]
+                // ,split 후에 11번째와 13번째 요소에서 위도(latitude), 경도(longitude) 를 알아냄
+
+                String latitude = splitStr[11].substring(splitStr[11].indexOf("=") + 1); // 위도
+                String longitude = splitStr[13].substring(splitStr[13].indexOf("=") + 1); // 경도
+
+                // 좌표(위도, 경도) 생성
+                LatLng point = new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude));
+                // 마커 생성
+                MarkerOptions mOptions2 = new MarkerOptions();
+                mOptions2.title("search result");
+                mOptions2.snippet(address);
+                mOptions2.position(point);
+                // 마커 추가
+                mGoogleMap.addMarker(mOptions2);
+                // 해당 좌표로 화면 줌
+                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 15));
+            }
+        });
+    }
+}
